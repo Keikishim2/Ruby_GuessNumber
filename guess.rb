564fe20c882d406e800cf62334756c61a17ab336
Keikishim2 <Keikishim2@gmail.com>
2021-06-21 18:45:19 +0800

    first

---
+++ guess.rb
@@ -0,0 +1,9 @@
+guess_number = 224
+number = 25
+if number == guess_number
+	puts "You got it, baby!"
+elsif number > guess_number
+	puts "Nope, too high!!"
+elsif number < guess_number
+	puts "Nope, too low!!"
+end
